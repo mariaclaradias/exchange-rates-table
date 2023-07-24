f8a3b0bc907fb07edc056cfa242fa29578905c25
Maria <maria.clr.dias@gmail.com>
2023-07-24 10:35:52 -0300

    feat: refresh button implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import RefreshButton from './components/refresh-button/RefreshButton';
 
 function App() {
-  return <div className="App"> </div>;
+  return (
+    <div className="App">
+      <RefreshButton onClick={() => {}} />
+    </div>
+  );
 }
 
 export default App;
